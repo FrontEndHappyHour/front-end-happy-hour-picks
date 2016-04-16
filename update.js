58b88dad438563ce2218a89809fe3165966628fb
ryanburgess <ryanb@netflix.com>
2016-04-16 12:51:22 -0700

    create JSON file of picks

--- update.js
+++ update.js
@@ -1,9 +1,11 @@
 'use strict';
 const request = require('request');
 const fs = require('fs');
+const jsonfile = require('jsonfile');
 const url = 'https://raw.githubusercontent.com/FrontEndHappyHour/website/master/content/episodes.json';
 const outputFile = './README.md';
 const top = '# Front End Happy Hour Picks\nEach episode of the [Front End Happy Hour](http://frontendhappyhour.com) podcast our panelists share interesting picks.';
+const list = [];
 let output = top;
 request({
     url: url,
@@ -15,7 +17,7 @@ request({
       const episode = data[i].episode;
       const published = data[i].published;
       const picks = data[i].picks;
-
+      const pickList = [];
       output += (`\n\n## Episode ${episode} - ${title}\nPublished: ${published}\n`);
 
       for(let a = 0; a < picks.length; a++){
@@ -24,7 +26,11 @@ request({
         const pickFrom = picks[a].from;
 
          output += (`- [${pickTitle}](${pickUrl}) - ${pickFrom}\n`);
+         pickList.push({'title': pickTitle, 'url': pickUrl, 'from': pickFrom});
       }
+
+      // update list for JSON file
+      list.push({'episode': `${episode} - ${title}`, 'published': published, 'picks': pickList});
     }
 
     // create updated readme
@@ -33,6 +39,15 @@ request({
         console.log(err);
       } else {
         console.log('Updated ' + outputFile);
+      }
+    });
+    console.log(list);
+    // create JSON of picks
+    jsonfile.writeFile('./list.json', list, {spaces: 2}, function(err) {
+      if(err) {
+        console.log(err);
+      }else {
+        console.log(`Updated list.json`);
       }
     });
   }
